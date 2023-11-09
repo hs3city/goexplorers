08190ed9ba68fc3ae1817f3da931ab01dba68f1d
Go Explorer <pyexplorer@hs3.pl>
2023-11-09 20:16:22 +0100

    session-5: add CSV parsing

--- quiz/main.go
+++ quiz/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
-
-func Hello(msg string) string {
-	return fmt.Sprintf("Hello %s", msg)
-}
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println(Hello("world"))
+	file, err := os.Open("quiz/problems.csv")
+	if err != nil {
+		newerr := fmt.Errorf("shit hit the fan: %v", err)
+		panic(newerr)
+	}
+	csvReader := csv.NewReader(file)
+
+	data, err := csvReader.ReadAll()
+
+	if err != nil {
+		newerr := fmt.Errorf("error while reading csv: %v", err)
+		panic(newerr)
+	}
+
+	for _, line := range data {
+		fmt.Printf("How much is %s ?\n", line[0])
+		var input int
+		fmt.Scan(&input)
+	}
 }
