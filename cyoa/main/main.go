ea5b833bb4fb4cbc498132dffff577786ca7ce8d
Go Explorer <pyexplorer@hs3.pl>
2023-12-07 20:44:50 +0100

    WIP Get key from a struct mapped from JSON

--- cyoa/main/main.go
+++ cyoa/main/main.go
@@ -2,38 +2,53 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
+	"reflect"
+	"strings"
 )
 
 func main() {
+	fmt.Println("Server starting...")
 	http.HandleFunc("/", handlerHttp)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handlerHttp(w http.ResponseWriter, r *http.Request) {
-	// fmt.Println(w, "our application, world")
+
+	pathSegments := strings.Split(r.URL.Path, "/")
+	expectedArc := pathSegments[1]
+
 	jsonInput, err := os.ReadFile("data.json")
 	if err != nil {
 		panic(err)
 	}
 
 	var story StoryLine
+
 	err = json.Unmarshal(jsonInput, &story)
 	if err != nil {
 		panic(err)
 	}
 
-	t := template.Must(template.New("new").ParseFiles("template.html"))
+	t, _ := template.ParseFiles("template.html")
 
-	err = t.Execute(w, story.Intro.Title)
+	val := reflect.ValueOf(story)
+	typ := reflect.TypeOf(story)
+	var retVal string
+
+	for i := 0; i < val.NumField(); i++ {
+		if typ.Field(i).Name == expectedArc {
+			fmt.Println(val.Field(i))
+			retVal = val.Field(i).String()
+		}
+	}
+
+	err = t.Execute(w, retVal)
 
 	if err != nil {
 		panic(err)
 	}
-
-	// w.Write([]byte(string(story.Intro.Title)))
-
-	// func setTemplate(){}
 }
