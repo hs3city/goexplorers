e5ece87764bc9bd449e8fbf039ff08b091b7b93a
Szymon Mikitiuk <singlet.bunches_0m@icloud.com>
2024-02-01 20:21:59 +0100

    add a complete solution for link exercise using the Tokenizer

--- link/main_test.go
+++ link/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -14,27 +13,6 @@ func Test_main(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			main()
-		})
-	}
-}
-
-func Test_catch(t *testing.T) {
-	type args struct {
-		val []byte
-		err error
-	}
-	tests := []struct {
-		name string
-		args args
-		want []byte
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := catch(tt.args.val, tt.args.err); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("catch() = %v, want %v", got, tt.want)
-			}
 		})
 	}
 }
