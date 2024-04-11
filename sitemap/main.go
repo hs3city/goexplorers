8cef3412e35c465b9f989768cc44c14caa51d65a
Py Explorer <pyexplorer@hs3.pl>
2024-04-11 19:48:53 +0200

    hehe

--- sitemap/main.go
+++ sitemap/main.go
@@ -7,17 +7,27 @@ import (
 	"os"
 )
 
-func w() {
-	//zrobmy
+type Data struct {
+	Lp   int
+	Body string
+}
+
+type Tree struct {
+	Lp   int
+	Body string
+}
+
+func w(path string) (*os.File, error) {
+	file, err := os.Open(path)
+	return file, err
 
 }
-func makeMeTree() {
-	//zrobmy
-
+func makeMeTree(data Data) Tree {
+	return Tree{}
 }
 
 func main() {
-	file, err := os.Open("./grandehtml.html")
+	file, err := w("./grandehtml.html")
 	if err != nil {
 		log.Fatalf("Error reading HTML file: %v", err)
 	}
